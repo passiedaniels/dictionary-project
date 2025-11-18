497322e8287c3c8be996a6fd18f0deb99c462c0e
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-11-18 18:28:31 +0300

    added api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
 
+  function handleDisplay(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyWord} defination`);
+
+    let apiKey = "943a3ddbo20b374aff624c0t29d891a1";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleDisplay);
   }
   function updateWord(event) {
     setKeyWord(event.target.value);
