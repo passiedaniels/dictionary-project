4d0aebddd30f9505d4050e9934d99b629538ddb2
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-11-18 23:48:59 +0300

    added  meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
+  let [results, setResults] = useState("null");
 
   function handleDisplay(response) {
     console.log(response.data);
+    setResults(response.data);
   }
-
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyWord} defination`);
 
     let apiKey = "943a3ddbo20b374aff624c0t29d891a1";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
@@ -35,6 +36,7 @@ export default function Dictionary() {
         />
         <input type="submit" value="search" className="submit-Button" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
